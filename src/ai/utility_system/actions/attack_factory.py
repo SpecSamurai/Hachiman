690447234255e745aa068c6ac4d484054ec9a3a6
Spec <dawid.spec.jachowicz@gmail.com>
2022-04-25 16:07:30 +0200

    Add AttackFactory class

---
+++ src/ai/utility_system/actions/attack_factory.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import ai.utility_system.worldstate as worldstate
+
+from ai.utility_system.actions.baseaction import BaseAction
+from sprites.unit import Unit
+
+
+class AttackFactory(BaseAction):
+    def __init__(self) -> None:
+        super().__init__(AttackFactory.__name__)
+
+    def run_script(self, worldstate: worldstate.WorldState) -> None:
+        unit: Unit = next(
+            filter(lambda unit: unit.status != Unit.COMBAT, worldstate.actor.units),
+            None,
+        )
+        if unit != None:
+            unit.attack(worldstate.enemy.factory)
+
+    def get_score(self, worldstate: worldstate.WorldState) -> int:
+        if len(worldstate.actor.units) >= 2 * len(worldstate.enemy.units):
+            return 5 + len(worldstate.actor.units) - len(worldstate.enemy.units)
+        else:
+            return 0
