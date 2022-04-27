8ba85edfe8ec01c9a78a833318dae180d3713126
Spec <dawidjachowicz@proton.me>
2022-04-27 09:22:22 +0200

    Refactor Resource class - Use the game_configuration constants

--- src/sprites/resource.py
+++ src/sprites/resource.py
@@ -1,13 +1,18 @@
 import arcade
 
+
 class Resource(arcade.SpriteCircle):
     RESOURCE_COLOR_RGB = (253, 120, 54)
 
-    RESOURCE_RADIUS = 50
+    RESOURCE_RADIUS = 35
     RESOURCE_ALPHA = 250
 
     def __init__(self, name: str, center_x: float = 0, center_y: float = 0) -> None:
-        super().__init__(radius=Resource.RESOURCE_RADIUS, color=Resource.RESOURCE_COLOR_RGB, soft=True)
+        super().__init__(
+            radius=Resource.RESOURCE_RADIUS,
+            color=Resource.RESOURCE_COLOR_RGB,
+            soft=True,
+        )
 
         self.name = name
         self.center_x = center_x
