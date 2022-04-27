629fe781b47827faab8b3a15bd986714c3ac4be0
Spec <dawid.spec.jachowicz@gmail.com>
2022-04-27 13:49:56 +0200

    Refactor Actor class - Use game_configuration constants - Add units_collisions_update method

--- src/actor.py
+++ src/actor.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
+import math
 import arcade
 
 from actorconfiguration import ActorConfiguration
+from game_configuration import (
+    FACTORY_BASE_HEALTH,
+    START_RESOURCES,
+    UNIT_BUILD_COST,
+    UNITS_LIMIT,
+)
 from sprites.factory import Factory
 from sprites.unit import Unit
 import ai.utility_system.worldstate as worldstate
@@ -12,7 +19,7 @@ from ai.utility_system.planner import Planner
 class Actor:
     def __init__(self, name: str, configuration: ActorConfiguration) -> None:
         self.name = name
-        self.resources = ActorConfiguration.START_RESOURCES
+        self.resources = START_RESOURCES
         self.configuration = configuration
 
         self.factory = Factory(
@@ -38,10 +45,7 @@ class Actor:
             self.plan = None
 
     def can_build_unit(self) -> bool:
-        return (
-            self.resources >= Unit.UNIT_BUILD_COST
-            and len(self.units) < ActorConfiguration.UNITS_LIMIT
-        )
+        return self.resources >= UNIT_BUILD_COST and len(self.units) < UNITS_LIMIT
 
     def move_unit(self, unit_name: str, position_x: float, position_y: float) -> None:
         unit: Unit = next(filter(lambda unit: unit.name == unit_name, self.units), None)
@@ -51,5 +55,15 @@ class Actor:
         unit: Unit = next(filter(lambda unit: unit.name == unit_name, self.units), None)
         unit.attack(target)
 
+    def units_collisions_update(self, unit) -> None:
+        collisions = arcade.check_for_collision_with_list(unit, self.units)
+
+        for collision in collisions:
+            x_diff = unit.center_x - collision.center_x
+            y_diff = unit.center_y - collision.center_y
+            angle = math.atan2(y_diff, x_diff)
+            unit.center_x = unit.center_x + math.cos(angle)
+            unit.center_y = unit.center_y + math.cos(angle)
+
     def get_status(self) -> str:
-        return f"{self.name}: {self.resources} | {len(self.units)}/{ActorConfiguration.UNITS_LIMIT} | {self.factory.health}/{Factory.BUILDING_BASE_HEALTH}"
+        return f"{self.name}: {self.resources} | {len(self.units)}/{UNITS_LIMIT} | {self.factory.health}/{FACTORY_BASE_HEALTH}"
