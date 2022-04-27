260466aae09cea31c3918d57c101e82475716217
Spec <dawidjachowicz@proton.me>
2022-04-27 09:24:34 +0200

    Remove constants from the ActorConfiguration class

--- src/actorconfiguration.py
+++ src/actorconfiguration.py
@@ -1,8 +1,4 @@
 class ActorConfiguration:
-    START_RESOURCES = 1000
-    UNITS_LIMIT = 20
-    DRONES_LIMIT = 10
-
     def __init__(self, building_sprite: str, unit_sprite: str, position) -> None:
         self.building_sprite = building_sprite
         self.unit_sprite = unit_sprite
