158e65b5e3590061ca620930a7e73c74686d0644
Spec <dawidjachowicz@proton.me>
2025-12-16 14:36:15 +0100

    Project restart

--- src/actorconfiguration.py
+++
@@ -1,5 +0,0 @@
-class ActorConfiguration:
-    def __init__(self, building_sprite: str, unit_sprite: str, position) -> None:
-        self.building_sprite = building_sprite
-        self.unit_sprite = unit_sprite
-        self.position = position
