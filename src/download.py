e68817a6f628c22a8671713c29fb11422bc0bc50
Spec <dawidjachowicz@proton.me>
2026-01-19 15:00:32 +0100

    Added download function

---
+++ src/download.py
@@ -0,0 +1,8 @@
+import urllib.request
+url = (
+    "https://raw.githubusercontent.com/rasbt/"
+    "LLMs-from-scratch/main/ch05/"
+    "01_main-chapter-code/gpt_download.py"
+)
+filename = url.split('/')[-1]
+urllib.request.urlretrieve(url, filename)
