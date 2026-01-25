df571ed3209b7d10909050628e0f587a6c487c76
Spec <dawidjachowicz@proton.me>
2026-01-25 15:17:33 +0100

    Added downloading finetunning dataset

---
+++ src/download_finetune.py
@@ -0,0 +1,31 @@
+import urllib.request
+import zipfile
+import os
+from pathlib import Path
+
+print("test")
+
+url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
+zip_path = "sms_spam_collection.zip"
+extracted_path = "sms_spam_collection"
+data_file_path = Path(extracted_path) / "SMSSpamCollection.tsv"
+
+print(data_file_path)
+
+def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
+    if data_file_path.exists():
+        print(f"{data_file_path} already exists. Skipping download and extraction.")
+        return
+
+    with urllib.request.urlopen(url) as response:
+        with open(zip_path, "wb") as out_file:
+            out_file.write(response.read())
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extracted_path)
+
+    original_file_path = Path(extracted_path) / "SMSSpamCollection"
+    os.rename(original_file_path, data_file_path)
+    print(f"File downloaded and saved as {data_file_path}")
+
+download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
